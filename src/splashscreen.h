329addd482b60a3694f8b72a4f59e62983fb8d1b
jchristm77 <jchristm77@yahoo.com>
2018-07-08 14:10:53 -0700

    Wrote out basic outline for splash screen header.

---
+++ src/splashscreen.h
@@ -0,0 +1,22 @@
+#ifndef SPLASHSCREEN_H
+#define SPLASHSCREEN_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+class QPushutton;
+QT_END_NAMESPACE
+
+class SplashScreen : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    mainWindow();
+
+private slots:
+
+private:
+
+
+};
