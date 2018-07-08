672d71e6d860e645a263bda12b86958e4971c112
jchristm77 <jchristm77@yahoo.com>
2018-07-08 14:20:40 -0700

    Created a basic main function to drive the app

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+/* Parts of this code are based on the "applicaton" example
+ * from the QT widgets tutorial */
+
+#include <QApplication>
+#include <QCommandLineOption>
+
+#include "splashscreen.h"
+
+int main(int argc, char ** argv)
+{
+    Q_INIT_RESOURCE(application);
+
+    QApplication app(argc, argv);
+    QCoreApplication::setApplicationName("Libre Cite");
+    QCoreApplication::setApplicationVersion(QT_VERSION_STR);
+
+    SplashScreen splashScreen;
+    splashScreen.show();
+    return app.exec();
+
+
+
+}
