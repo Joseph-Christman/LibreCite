797a2df5d16935f7a19a8baaa7c1f96eb3bb8ffe
jchristm77 <jchristm77@yahoo.com>
2018-07-12 12:14:13 -0700

    Basic boilerplate for citationselectionwindow

---
+++ src/citationselectionwindow.cpp
@@ -0,0 +1,8 @@
+#include <QtWidgets>
+
+#include "citationselectionwindow.h"
+
+CitationSelectionWindow::CitationSelectionWindow(QWidget * parent)
+{
+
+}
---
+++ src/citationselectionwindow.h
@@ -0,0 +1,21 @@
+#ifndef CITATIONSELECTIONWINDOW_H
+#define CITATIONSELECTIONWINDOW_H
+
+#include <QWindow>
+
+QT_BEGIN_NAMESPACE
+
+QT_END_NAMESPACE
+
+class CitationSelectionWindow : public QWindow
+{
+    Q_OBJECT
+
+public:
+    CitationSelectionWindow(QWidget * parent);
+
+private:
+
+};
+
+#endif
