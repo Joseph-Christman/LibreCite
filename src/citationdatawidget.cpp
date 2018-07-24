1d5de89cd78177b410c2c8546c2649f3d5da5edf
jchristm77 <jchristm77@yahoo.com>
2018-07-24 09:51:29 -0700

    Built more plausible window for book citations

--- src/citationdatawidget.cpp
+++ src/citationdatawidget.cpp
@@ -16,11 +16,27 @@ void CitationDataWidget::setBookLayout()
 {
     QGridLayout * mainLayout = new QGridLayout;
 
-    QLabel * titleLabel = new QLabel(tr("Title: "));
-    QTextEdit * titleText = new QTextEdit;
+    QLabel * titleLabel = new QLabel(tr("Title "));
+    QLineEdit * titleText = new QLineEdit;
+
+    QLabel * authorLabel = new QLabel(tr("Author "));
+    QLineEdit * authorText = new QLineEdit;
+    
+    QLabel * yearLabel = new QLabel(tr("Year "));
+    QLineEdit * yearText = new QLineEdit;
+
+
+    QPushButton * submitButton = new QPushButton(tr("&Submit"));
 
     mainLayout->addWidget(titleLabel, 0, 0);
     mainLayout->addWidget(titleText, 0, 1);
+
+    mainLayout->addWidget(authorLabel, 1, 0);
+    mainLayout->addWidget(authorText, 1, 1);
+
+    mainLayout->addWidget(yearLabel, 2, 0);
+    mainLayout->addWidget(yearText, 2, 1);
+    mainLayout->addWidget(submitButton, 3, 0);
 
     setLayout(mainLayout);
 }
